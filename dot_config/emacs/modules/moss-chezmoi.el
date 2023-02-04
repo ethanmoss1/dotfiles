bb64802bc976720da649f70071ee5a1208e87de8
Ethan Moss <cywinskimoss@gmail.com>
2023-02-04 21:18:07 +0000

    Add .config/chezmoi/chezmoi.toml Update .config/emacs/modules/moss-chezmoi.el Change attributes of .config/alacritty/alacritty.yml

--- dot_config/emacs/modules/moss-chezmoi.el
+++ dot_config/emacs/modules/moss-chezmoi.el
@@ -7,6 +7,12 @@
 (message "[ Moss ] Loading module, chezmoi  ... ")
 
 (use-package chezmoi
+  :config
+  (require 'chezmoi-company)
+  (add-hook 'chezmoi-mode-hook #'(lambda ()
+            (if chezmoi-mode
+                (add-to-list 'company-backends 'chezmoi-company-backend)
+              (delete 'chezmoi-company-backend 'company-backends))))
   )
 
 (provide 'moss-chezmoi)
