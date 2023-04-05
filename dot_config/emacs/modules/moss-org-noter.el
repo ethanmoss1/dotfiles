1c5a5dfa5c7822104ba68908f7afa843694b524c
Ethan Moss <cywinskimoss@gmail.com>
2023-04-05 12:24:29 +0100

    Update .config/emacs/modules/moss-org-noter.el Update .config/i3/config Add Documents/Org/Applications/alacritty.org Add Documents/Org/Applications/bitwarden.org Add Documents/Org/Applications/bluetooth.org Add Documents/Org/Applications/isync.org Add Documents/Org/Finances.org Add Documents/Org/Journal.org Add Documents/Org/Metrics.org Add Documents/Org/Tasks.org Add Documents/Org/org-roam/#20220928175012-programming_language.org# Add Documents/Org/org-roam/20220928162916-org_roam.org Add Documents/Org/org-roam/20220928163630-zettelkasten.org Add Documents/Org/org-roam/20220928163852-emacs.org Add Documents/Org/org-roam/20220928164158-org_mode.org Add Documents/Org/org-roam/20220928174932-c.org Add Documents/Org/org-roam/20220928175012-programming_language.org Add Documents/Org/org-roam/20220928175046-emacs_lisp.org Add Documents/Org/org-roam/20220928175130-common_lisp.org Add Documents/Org/org-roam/20220928185119-python.org Add Documents/Org/org-roam/20220928200221-open_university.org Add Documents/Org/org-roam/20220928200322-computing_and_it.org Add Documents/Org/org-roam/20221003113022-tm111.org Add Documents/Org/org-roam/20221003114430-icma.org Add Documents/Org/org-roam/20221003114600-tma.org Add Documents/Org/org-roam/20221004095144-telephone.org Add Documents/Org/org-roam/20221004095504-network.org Add Documents/Org/org-roam/20221004115317-rss.org Add Documents/Org/org-roam/20221004134225-subscriber_trunk_dialing.org Add Documents/Org/org-roam/20221004134644-computer.org Add Documents/Org/org-roam/20221004134908-cobol.org Add Documents/Org/org-roam/20221013124444-colossus.org Add Documents/Org/org-roam/20221013124530-ww2.org Add Documents/Org/org-roam/20221013124608-enigma.org Add Documents/Org/org-roam/20221013124925-electronic_switches.org Add Documents/Org/org-roam/20221013125052-binary_number.org Add Documents/Org/org-roam/20221013125201-electronic_valves.org Add Documents/Org/org-roam/20221013125414-transistor.org Add Documents/Org/org-roam/20221013130002-eniac.org Add Documents/Org/org-roam/20221013132529-decimal_numbers.org Add Documents/Org/org-roam/20221013132926-data.org Add Documents/Org/org-roam/20221013133126-typewriter.org Add Documents/Org/org-roam/20221013140437-program.org Add Documents/Org/org-roam/20221101123210-audacity.org Add Documents/Org/org-roam/20221101123602-data_protection.org Add Documents/Org/org-roam/20221101141924-psychoacoustics.org Add Documents/Org/org-roam/20230124120156-fibre_optic.org Add Documents/Org/org-roam/20230124121935-electromagnetic_radiation.org Add Documents/Org/org-roam/20230124121953-sine_wave.org Add Documents/Org/org-roam/20230124122305-wavelength.org Add Documents/Org/org-roam/daily/2022-09-28.org Add Documents/Org/org-roam/daily/2022-09-29.org Add Documents/Org/org-roam/daily/2022-10-01.org Add Documents/Org/org-roam/daily/2022-10-03.org

--- dot_config/emacs/modules/moss-org-noter.el
+++ dot_config/emacs/modules/moss-org-noter.el
@@ -1,7 +1,8 @@
 ;;; moss-org-noter.el --- Summary
 
 ;;; Commentary : ---------------------------------------------------------------
-
+;; Org noter allows you to have a specific org file for your notes on pdf
+;; documents
 ;;; Code : ---------------------------------------------------------------------
 
 (message "[ Moss ] Loading module, org-noter  ... ")
