6b6a2971d8bc26a0a3dcdb723589cb692fbf6869
Ethan Moss <cywinskimoss@gmail.com>
2023-05-05 10:30:22 +0100

    elpaca cache directory incorrect

--- dot_config/emacs/core/moss-package-manager-elpaca.el
+++ dot_config/emacs/core/moss-package-manager-elpaca.el
@@ -28,9 +28,9 @@
 
 ;; Set up Elpaca variables
 (defvar elpaca-installer-version 0.4)
-(defvar elpaca-directory (expand-file-name "cache/elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "cache/builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "cache/repos/" elpaca-directory))
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil
                               :files (:defaults (:exclude "extensions"))
