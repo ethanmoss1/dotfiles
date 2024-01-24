90ea9584d29ad96eb4465787a3614f60f7da9b22
Ethan Moss <cywinskimoss@gmail.com>
2024-01-24 11:18:11 +0000

    fixed bug with testing system type

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -38,14 +38,15 @@
                     "-name" "*.eln.tmp" "-size" "0" "-delete"))))
 
 (if (eq system-type 'android)
-    (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
-		       (getenv "PATH")))
-    (setenv "LD_LIBRARY_PATH" (format "%s:%s"
-				  "/data/data/com.termux/files/usr/lib"
-				  (getenv "LD_LIBRARY_PATH")))
-    (push "/data/data/com.termux/files/usr/bin" exec-path)
-    (setenv "HOME" "/data/data/com.termux/files/home/")
-    (setq user-emacs-directory "~/.config/emacs/"))
+    (progn
+      (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
+			     (getenv "PATH")))
+      (setenv "LD_LIBRARY_PATH" (format "%s:%s"
+					"/data/data/com.termux/files/usr/lib"
+					(getenv "LD_LIBRARY_PATH")))
+      (push "/data/data/com.termux/files/usr/bin" exec-path)
+      (setenv "HOME" "/data/data/com.termux/files/home/")
+      (setq user-emacs-directory "~/.config/emacs/")))
 
 
 (setq package-enable-at-startup nil)
