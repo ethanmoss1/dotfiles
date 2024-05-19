c8e2079a7a21fdad34d03e3b9f457c355b37ccb6
Ethan Moss <cywinskimoss@gmail.com>
2024-05-19 21:17:02 +0100

    FEAT: systemd unit files

---
+++ dot_config/emacs/modules/completion/moss-systemd.el
@@ -0,0 +1,12 @@
+;;; moss-systemd.el --- Summary
+
+;;; Commentary : ---------------------------------------------------------------
+
+;;; Code : ---------------------------------------------------------------------
+
+(message "[ Moss ] Loading module, systemd  ... ")
+
+(use-package systemd)
+
+(provide 'moss-systemd)
+;;; moss-systemd.el ends here -----------------------------------------------------
