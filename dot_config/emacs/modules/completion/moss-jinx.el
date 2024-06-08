5e173320ee4ecc5b285ae8bdd90a9bb715d10589
Ethan Moss <cywinskimoss@gmail.com>
2024-06-08 13:52:24 +0100

    FEAT: Additional modules
    Added some more modules to mess around with, jinx is something work checking out
    more. Also need to look into GTD more

---
+++ dot_config/emacs/modules/completion/moss-jinx.el
@@ -0,0 +1,13 @@
+;;; moss-jinx.el --- Summary
+
+;;; Commentary : ---------------------------------------------------------------
+;; A spell checking modules that can work from fontlock. allowing on the fly
+;; spell checking in comments and docstrings.
+
+;;; Code : ---------------------------------------------------------------------
+(message "[ Moss ] Loading module, jinx  ... ")
+
+(use-package jinx)
+
+(provide 'moss-jinx)
+;;; moss-jinx.el ends here -----------------------------------------------------
---
+++ dot_config/emacs/modules/org/moss-org-gtd.el
@@ -0,0 +1,27 @@
+;;; moss-org-gtd.el --- Summary
+
+;;; Commentary : ---------------------------------------------------------------
+
+;;; Code : ---------------------------------------------------------------------
+
+(message "[ Moss ] Loading module, org-gtd  ... ")
+
+(use-package org-gtd
+  :ensure (org-gtd.el :host github :repo "Trevoke/org-gtd.el")
+  :after org
+  :custom
+  ;;; GTD Meta
+  (org-gtd-directory "~/Org")
+  ;; (org-gtd-areas-of-focus ’("list of strings"))
+  ;; (org-gtd-)
+
+  ;;; GTD Capture
+  ;; (org-gtd-capture-templates) ;; see org capture templates
+
+  ;;; GTD Process
+
+  ;;; GTD Clarify
+  )
+
+(provide 'moss-org-gtd)
+;;; moss-org-gtd.el ends here -----------------------------------------------------
