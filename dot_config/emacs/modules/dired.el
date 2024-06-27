859ab233a3a90a5833b8e53fe91e40757c5ec852
Ethan Moss <cywinskimoss@gmail.com>
2024-06-27 17:03:45 +0100

    Dired module fully refactored, added additional settings

--- dot_config/emacs/modules/dired.el
+++ dot_config/emacs/modules/dired.el
@@ -1,11 +1,33 @@
-;;; dired --- Summary
+;;; dired.el --- Directory Editor  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Ethan Moss
+
+;; Author: Ethan Moss <cywinskimoss@gmail.com>
+;; Keywords: directories dir editing files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+;; Directory Editor
+
+;; Dired makes an Emacs buffer containing a listing of a directory,
+;; and optionally some of its subdirectories as well.  You can use the
+;; normal Emacs commands to move around in this buffer, and special Dired
+;; commands to operate on the listed files.  Dired works with both local and
+;; remote directories.
 
 ;;; Code:
-
-(message "[ Moss ] Loading module, dired  ... ")
-
 (use-package dired
   :ensure nil
   :defer t
@@ -15,14 +37,13 @@
         ("<right>" . dired-find-file))
 
   :config
-  (setq dired-listing-switches "-laGvh --group-directories-first")
+  (setq dired-listing-switches "-laGvh --group-directories-first"
+		dired-auto-revert-buffer 'dired-directory-changed-p
+		dired-clean-confirm-killing-deleted-buffers t
+		dired-clean-up-buffers-too t
+		dired-create-destination-dirs 'always
+		dired-free-space 'separate
+		dired-mouse-drag-files 'move))
 
-  :hook
-  (dired-mode . moss-dired-mode-setup)
-
-  :init
-  (defun moss-dired-mode-setup ()
-    (hl-line-mode +1)))
-
-(provide 'moss-dired)
-;;; moss-dired.el ends here
+(provide 'dired)
+;;; dired.el ends here
