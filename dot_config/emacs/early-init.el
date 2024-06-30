6522e8168ba8b358ed0030bd2344bdcb8395d805
Ethan Moss <cywinskimoss@gmail.com>
2024-06-30 15:23:51 +0100

    FIX: bring inline with other files

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -1,3 +1,24 @@
+;;; early-init.el --- -*- lexical-binding: t;-*-
+
+;; Copyright (C) 2024  Ethan Moss
+
+;; Author: Ethan Moss <cywinskimoss@gmail.com>
+;; Keywords: emacs init early
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
 ;; Native-Comp
 (setq native-comp-speed 2
       comp-speed 2)
@@ -26,8 +47,10 @@
                     "-name" "*.eln" "-size" "0" "-delete" "-or"
                     "-name" "*.eln.tmp" "-size" "0" "-delete"))))
 
+;; Turn off Package.el, we use our own package manager
 (setq package-enable-at-startup nil)
 
+;; remove some features
 (setq tool-bar-mode nil)
 (menu-bar-mode -1)
 (when (fboundp 'set-scroll-bar-mode)
