793f992ffea42ebb160b49889324ee1022ef4c1c
Ethan Moss <cywinskimoss@gmail.com>
2024-09-09 20:12:04 +0100

    FEAT: org

--- dot_config/emacs/modules/org.el
+++ dot_config/emacs/modules/org.el
@@ -26,7 +26,7 @@
   (interactive)
   (org-latex-preview '(16)))
 
-(defun moss/reload-org-agenda-files ()
+(defun my/reload-org-agenda-files ()
   "Reloads the agenda file list"
   (interactive)
   (setq org-agenda-files (directory-files-recursively "~/documents/org" "\\.org$")))
@@ -48,49 +48,28 @@
   (insert "\\]")
   (previous-line))
 
-
-;; (defun org--get-display-dpi ()
-;;   "MODIFIED FOR TABLET - MOSS
-;; Get the DPI of the display.
-;; The function assumes that the display has the same pixel width in
-;; the horizontal and vertical directions."
-;;   266)
-
 (use-package org
   :hook (org-mode . my/load-minor-modes-for-org)
   :bind
   ("C-c o a" . 'org-agenda)
   ("C-c o c" . 'org-capture)
   :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages '((python . t)
-                               (gnuplot . t)
-                               (maxima . t)
-                               (shell . t)
-                               (C . t)))
+  (org-babel-do-load-languages 'org-babel-load-languages '((python . t)
+														   (gnuplot . t)
+														   (maxima . t)
+														   (shell . t)
+														   (C . t)))
 
-  ;; :custom-face
-  ;; (org-level-1 ((t (:inherit outline-1 :height 1.75))))
-  ;; (org-level-2 ((t (:inherit outline-2 :height 1.5))))
-  ;; (org-level-3 ((t (:inherit outline-3 :height 1.25))))
-  ;; (org-level-4 ((t (:inherit outline-4 :height 1.1))))
-  ;; (org-document-title ((t (:height 2.0))))
-
-  :custom
-  (org-directory "~/documents/org/")
-  (org-agenda-files (directory-files-recursively "~/documents/org" "\\.org$"))
-  ;; (org-format-latex-options (plist-put org-format-latex-options :scale 1))
-  (org-hide-emphasis-markers t)
-  ;; (org-refile-targets
-  ;;     '((nil :maxlevel . 3)
-  ;;       (org-agenda-files :maxlevel . 3)))
-
-  (org-capture-templates
-   `(("i" "Inbox" entry (file "inbox.org")
-      ,(s-join "\n" '("* TODO %?"
-                       ":PROPERTIES:"
-                       ":ENTERED: %U"
-                       ":FILE: [[%F]]"
-                       ":END:"))))))
+  (setq org-directory "~/documents/org/"
+		org-agenda-files (directory-files-recursively "~/documents/org"
+													  "\\.org$")
+		org-todo-keywords '("TODO(t)" "NEXT(n!)" "|" "DONE(d!@)")
+		org-hide-emphasis-markers t
+		org-capture-templates `(("i" "Inbox" entry (file "inbox.org")
+								 ,(s-join "\n" '("* TODO: %?"
+												 ":PROPERTIES:"
+												 ":ENTERED: %U"
+												 ":FILE: [[%F]]"
+												 ":END:"))))))
 
 ;;; org.el ends here
