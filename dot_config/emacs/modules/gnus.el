f479b24ad708e0a194122a84a8b5b16db14d9dda
Ethan Moss <cywinskimoss@gmail.com>
2024-09-13 09:26:36 +0100

    FEAT: incomplete gnus setup

--- dot_config/emacs/modules/gnus.el
+++ dot_config/emacs/modules/gnus.el
@@ -3,20 +3,47 @@
 ;;; Commentary : ---------------------------------------------------------------
 
 ;;; Code : ---------------------------------------------------------------------
-
-(message "[ Moss ] Loading module, gnus  ... ")
-
 (use-package gnus
-  :ensure nil
-  :custom
-  (user-mail-address "wow.ethm@gmail.com")
-  (user-full-name    "Ethan Moss")
+  :ensure (:package "gnus"
+		   ;; :depth 1
+		   :inherit nil
+		   :protocol https
+		   :files (:defaults "lisp/gnus/*" (:exclude ".git"))
+		   :repo "https://git.savannah.gnu.org/git/emacs.git")
+  :config
+  (setq user-full-name "Ethan Moss"
+		gnus-select-method '(nntp "news.eternal-september.org"))
 
-  (smtpmail-smtp-server  "smtp.gmail.com")
-  (smtpmail-smtp-service 587)
-  (gnus-select-method '(nnimap "imap.gmail.com"))
+  (add-to-list 'gnus-secondary-select-methods
+               '(nnatom "learn2.open.ac.uk/blocks/news/feed.php?bi=304299"
+						(nnatom-name "M269 News")))
+  (add-to-list 'nnrss-group-alist '(("xkcd.com"
+									"https://xkcd.com/rss.xml"
+									"A webcomic of romance and math humor."))))
 
-  (send-mail-function 'smtpmail-send-it))
 
-(provide 'moss-gnus)
-;;; moss-TEMP.el ends here -----------------------------------------------------
+;; https://xkcd.com/rss.xml
+;; (nnrss "https://emacs.org/rss/emacs.xml"
+;;				  (nnrss-name "Emacs News"))
+
+
+;;;; Welcome as a new user of news.eternal-september.org
+;; You have successfully registered as a user of the news.eternal-september.org
+;; Usenet news server.  As a registered user you may now access all newsgroups
+;; and post articles to these groups.  Please configure your newsreader to
+;; access
+;;
+;; news.eternal-september.org
+;;
+;; The port number is 119. This is the standard port number for news servers and
+;; should be set as a default in your client. You also have to configure your
+;; client to use the userid and password you will soon receive by email.  If you
+;; are only able to select groups in the;
+;;
+;; eternal-september.* and posenet.*
+;;
+;; hierarchies, your news client has connected to the server without sending the
+;; userid and password. Please refer to your newsreader's documentation for
+;; information on how to configure server access with authentication or check
+;; the FAQ In case of problems or questions don't hesitate to conatct
+;; support@eternal-september.org.
