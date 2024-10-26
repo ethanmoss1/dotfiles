fe0e563b23d5ef5b1257408bf801ec8ebd3d9692
Ethan Moss <cywinskimoss@gmail.com>
2024-10-26 17:41:57 +0100

    FEAT: added EDNC and built in calendar

--- dot_config/emacs/modules-list.el
+++ dot_config/emacs/modules-list.el
@@ -1,84 +1,88 @@
 ;; Load modules
-(mapcar 'module-load
-        '(;;;;;; ---- List of modules to load ----
-          ;; -- Package manager --
-          "elpaca"
+(mapcar
+ 'module-load
+ '(;;;;;; ---- List of modules to load ----
+   ;; -- Package manager --
+   "elpaca"
 
-          ;; -- Built in modules --
-          "c-code"
-          "startup"
-          "frame"
-          "mouse"
-          "widget"
-          "simple"
-          "electric"
-          "font-core"
-          "saveplace"
-          "subr"
-          "files"
-          "delsel"
-          "paren"
-          "uniquify"
-		  "recentf"
-          "savehist"
-		  "auto-insert"
-          "dired"
-          "keybindings"
-		  "ibuffer"
-		  "window"
-		  "tramp"
-		  ;; "gnus"
-		  "ispell"
-		  "minibuffer"
+   ;; -- Built in modules --
+   "c-code"
+   "startup"
+   "frame"
+   "mouse"
+   "widget"
+   "simple"
+   "electric"
+   "font-core"
+   "saveplace"
+   "subr"
+   "files"
+   "delsel"
+   "paren"
+   "uniquify"
+   "recentf"
+   "savehist"
+   "auto-insert"
+   "dired"
+   "keybindings"
+   "ibuffer"
+   "window"
+   "tramp"
+   ;; "gnus"
+   "ispell"
+   "minibuffer"
 
-          ;; -- Compulsory Modules --
-          "vertico"
-          "orderless"
-          "marginalia"
-          "chezmoi"
+   ;; -- Compulsory Modules --
+   "vertico"
+   "orderless"
+   "marginalia"
+   "chezmoi"
 
-		  ;; Window manager and accompying modules
-		  ;; Will only load when ‘my-hostname’ is equal to laptop
-		  "desktop-environment"
-		  "xelb"
-		  "exwm"
-		  "app-launcher"
-		  "buffer-move"
-		  "tab-bar"
-		  "display-time"
-		  "pdf-tools"
+   ;; Window manager and accompying modules
+   ;; Will only load when ‘my-hostname’ is equal to laptop
+   "desktop-environment"
+   "xelb"
+   "exwm"
+   "app-launcher"
+   "buffer-move"
+   "tab-bar"
+   "display-time"
+   "pdf-tools"
+   "ednc"
 
-          ;; -- Themeing Modules --
-          "alltheicons"
-		  "modus-themes"
-		  ;; "gruvbox"
-		  "moodline"
+   ;; -- Themeing Modules --
+   "alltheicons"
+   "modus-themes"
+   ;; "gruvbox"
+   "moodline"
 
-          ;; Org Extras
-          "org"
-		  "org-caldav"
-          "org-roam"
-          "org-fragtog"
-          ;; "org-noter"
-          ;; "org-gtd"
+   ;; Org Extras
+   "org"
+   "org-caldav"
+   "org-roam"
+   "org-fragtog"
+   ;; "org-noter"
+   ;; "org-gtd"
 
-		  ;; -- News and Email --
-		  "elfeed"
-		  "notmuch"
+   ;; -- News and Email --
+   "elfeed"
+   "notmuch"
 
-          ;; -- Editing modules --
-          "programming"
-          "vundo"  ;; Visual undo history
-          "hl-todo"
-          "corfu"
-          "rainbow"
-          "consult"
+   ;; -- Editing modules --
+   "programming"
+   "vundo"  ;; Visual undo history
+   "hl-todo"
+   "corfu"
+   "rainbow"
+   "consult"
 
-          ;; -- Version Control --
-          "magit"
+   ;; -- Version Control --
+   "magit"
 
-          ;; -- Other modules --
-		  "helpful"
-          "docker"
-		  "nix"
-          )) ;; -- End of Modules --
+   ;; -- Other modules --
+   "helpful"
+   "docker"
+   "nix"
+   "eat"
+
+   )) ;; -- End of Modules --
