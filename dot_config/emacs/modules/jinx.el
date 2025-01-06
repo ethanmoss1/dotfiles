f043678eedd010532cb6ae63de8724ce2bc3d19d
Ethan Moss <cywinskimoss@gmail.com>
2025-01-06 20:24:14 +0000

    FIX: jinx
    updated jinx to be of the new module style
    added checks for outside dependencys

--- dot_config/emacs/modules/jinx.el
+++ dot_config/emacs/modules/jinx.el
@@ -1,13 +1,41 @@
-;;; moss-jinx.el --- Summary
+;;; jinx.el --- Just in time spell checker           -*- lexical-binding: t; -*-
 
-;;; Commentary : ---------------------------------------------------------------
-;; A spell checking modules that can work from fontlock. allowing on the fly
-;; spell checking in comments and docstrings.
+;; Copyright (C) 2024  Ethan Moss
 
-;;; Code : ---------------------------------------------------------------------
-(message "[ Moss ] Loading module, jinx  ... ")
+;; Author: Ethan Moss <ethan@Ethans-MacBook-Pro.local>
+;; Keywords: lisp
 
-(use-package jinx)
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-(provide 'moss-jinx)
-;;; moss-jinx.el ends here -----------------------------------------------------
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; TODO:
+;;; Code:
+
+;; Make sure to install enchant-2 and pkgconf on you system
+(let ((enchant (executable-find "enchant-2"))
+       (pkg-conf (executable-find "pkgconf")))
+  (if (and enchant pkg-conf)
+      (use-package jinx
+        :hook (elpaca-after-init . global-jinx-mode)
+        :bind (("C-." . jinx-correct)
+               ("C->" . jinx-languages)))
+    (message "Jinx Not Loaded: Cannot find executable(s): %s"
+             (concat (if (not enchant) "enchant-2 ")
+                     (if (not pkg-conf) "pkgconf ")))))
+
+
+;; This is a badly spelt word, I hope is can correctly fix these words.
+
+
+;;; jinx.el ends here
