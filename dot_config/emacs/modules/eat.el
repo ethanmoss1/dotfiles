e2ca1ab9a2213a7c116710d9bbadc6906f3727fb
Ethan Moss <cywinskimoss@gmail.com>
2025-01-23 17:26:04 +0000

    FIX: shell popup
    moved display buffer alist out of the eat use package macro

--- dot_config/emacs/modules/eat.el
+++ dot_config/emacs/modules/eat.el
@@ -41,17 +41,17 @@
   ;; For `eat-eshell-visual-command-mode'.
   ;; (add-hook 'eshell-load-hook #'eat-eshell-visual-command-mode)
 
-  (eat-eshell-mode)
+  (eat-eshell-mode))
 
-  ;; Set up the popup shell
-  (add-to-list 'display-buffer-alist
-               ;; *shell*  *eshell*  *eat*
-               '("\\*\\(e?shell\\|eat\\)\\*"
-                 (display-buffer-in-side-window)
-                 (side . bottom)
-                 (slot . -1) ;; -1 == L  0 == Mid 1 == R
-                 (window-height . 0.33) ;; take 2/3 on bottom left
-                 (window-parameters
-                  (no-delete-other-windows . nil)))))
+;; Set up the popup shell
+(add-to-list 'display-buffer-alist
+             ;; *shell*  *eshell*  *eat*
+             '("\\*\\(e?shell\\|eat\\)\\*"
+               (display-buffer-in-side-window)
+               (side . bottom)
+               (slot . -1) ;; -1 == L  0 == Mid 1 == R
+               (window-height . 0.33) ;; take 2/3 on bottom left
+               (window-parameters
+                (no-delete-other-windows . nil))))
 
 ;;; eat.el ends here
