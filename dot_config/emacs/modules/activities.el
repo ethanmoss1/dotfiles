cbcaef2a325b7c8a499b7383a604704f18042a15
Ethan Moss <cywinskimoss@gmail.com>
2025-01-29 20:58:07 +0000

    NEW MODULE: Activites
    This module allows for saving of "activites"

---
+++ dot_config/emacs/modules/activities.el
@@ -0,0 +1,43 @@
+;;; activities.el --- Manage frames and tabs         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Ethan Moss
+
+;; Author: Ethan Moss <ethan@Ethans-MacBook-Pro.local>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(use-package activities
+  :init
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+  :bind
+  (("C-x C-a C-n" . activities-new)
+   ("C-x C-a C-d" . activities-define)
+   ("C-x C-a C-a" . activities-resume)
+   ("C-x C-a C-s" . activities-suspend)
+   ("C-x C-a C-k" . activities-kill)
+   ("C-x C-a RET" . activities-switch)
+   ("C-x C-a b" . activities-switch-buffer)
+   ("C-x C-a g" . activities-revert)
+   ("C-x C-a l" . activities-list))
+  :config
+  (activities-mode))
+
+;;; activities.el ends here
+;; Local Variables:
+;; eval: (if config-module-managed-dotfiles (add-hook 'after-save-hook 'chezmoi-write nil t))
+;; End:
