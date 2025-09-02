aa34241d1d86c130b81076ff1548632c97f08494
Ethan Moss <cywinskimoss@gmail.com>
2025-09-02 14:35:19 +0100

    FIX: pdf-tools.el - install correctly
    Added the correct mumbo as well as having a hook to install pdftools and only if
    in a graphical environment

--- dot_config/emacs/modules/pdf-tools.el
+++ dot_config/emacs/modules/pdf-tools.el
@@ -1,13 +1,28 @@
-;;; pdf-tools.el --- Summary
+;;; pdf-tools.el --- Display PDFs in Emacs           -*- lexical-binding: t; -*-
+;; Copyright (C) 2025  Ethan Moss
 
-;;; Commentary:
+;; Author: Ethan Moss <cywinskimoss@gmail.com>
+;; Keywords: lisp
 
-;;; Code:
-(use-package pdf-tools
-  ;; :if (string-equal my-hostname "laptop")
-  :config
-  (pdf-loader-install)
-  (setq pdf-view-display-size 'fit-page))
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code :
+(if (display-graphic-p)
+    (use-package pdf-tools
+      :hook (elpaca-after-init . #'pdf-loader-install)
+      :config
+      (setq pdf-view-display-size 'fit-page)))
 
 ;;; pdf-tools.el ends here
 ;; Local Variables:
