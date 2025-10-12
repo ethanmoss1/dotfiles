6f40dbcef722f05592453d60fcc3a175322748c6
Ethan Moss <cywinskimoss@gmail.com>
2025-10-12 16:30:04 +0100

    FIX: Corfu with a bar in exwm

--- dot_config/emacs/modules/corfu.el
+++ dot_config/emacs/modules/corfu.el
@@ -70,38 +70,40 @@
 
   ;; Enable indentation+completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
-  (setopt tab-always-indent 'complete)
+  (setopt tab-always-indent 'complete))
 
-  ;; Add advise around corfu if we are using EXWM and a bar
-  (when (fboundp 'exwm-mode)
-    (defun get-focused-monitor-geometry ()
-      "Get the geometry of the monitor displaying the selected frame in EXWM."
-      (let* ((monitor-attrs (frame-monitor-attributes))
-             (workarea (assoc 'workarea monitor-attrs))
-             (geometry (cdr workarea)))
-        (list (nth 0 geometry) ; X
-              (nth 1 geometry) ; Y
-              (nth 2 geometry) ; Width
-              (nth 3 geometry) ; Height
-              )))
+(use-package emacs
+  :ensure nil
+  :after (corfu exwm)
+  :config
+  (defun get-focused-monitor-geometry ()
+    "Get the geometry of the monitor displaying the selected frame in EXWM."
+    (let* ((monitor-attrs (frame-monitor-attributes))
+           (workarea (assoc 'workarea monitor-attrs))
+           (geometry (cdr workarea)))
+      (list (nth 0 geometry) ; X
+            (nth 1 geometry) ; Y
+            (nth 2 geometry) ; Width
+            (nth 3 geometry) ; Height
+            )))
 
-    (defun advise-corfu-make-frame-with-monitor-awareness (orig-fun frame x y width height)
-      "Advise `corfu--make-frame` to be monitor-aware, adjusting X and Y according to the focused monitor."
+  (defun advise-corfu-make-frame-with-monitor-awareness (orig-fun frame x y width height)
+    "Advise `corfu--make-frame` to be monitor-aware, adjusting X and Y according to the focused monitor."
 
-      ;; Get the geometry of the currently focused monitor
-      (let* ((monitor-geometry (get-focused-monitor-geometry))
-             (monitor-x (nth 0 monitor-geometry))
-             (monitor-y (nth 1 monitor-geometry))
-             (selected-frame-position (frame-position))
-             (selected-frame-x (car selected-frame-position))
-             (selected-frame-y (cdr selected-frame-position))
-             (new-x (+ monitor-x selected-frame-x x))
-             (new-y (+ monitor-y selected-frame-y y)))
+    ;; Get the geometry of the currently focused monitor
+    (let* ((monitor-geometry (get-focused-monitor-geometry))
+           (monitor-x (nth 0 monitor-geometry))
+           (monitor-y (nth 1 monitor-geometry))
+           (selected-frame-position (frame-position))
+           (selected-frame-x (car selected-frame-position))
+           (selected-frame-y (cdr selected-frame-position))
+           (new-x (+ monitor-x selected-frame-x x))
+           (new-y (+ monitor-y selected-frame-y y)))
 
-        ;; Call the original function with potentially adjusted coordinates
-        (funcall orig-fun frame new-x new-y width height)))
+      ;; Call the original function with potentially adjusted coordinates
+      (funcall orig-fun frame new-x new-y width height)))
 
-    (advice-add 'corfu--make-frame :around #'advise-corfu-make-frame-with-monitor-awareness)))
+  (advice-add 'corfu--make-frame :around #'advise-corfu-make-frame-with-monitor-awareness))
 
 ;;; corfu.el ends here ----------------------------------------------------
 ;; Local Variables:
